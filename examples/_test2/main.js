fda02f7f7fad626d50bc15ba9d3b29d2bb95aed9
hta393939 <hta393939@gmail.com>
2025-11-16 17:21:35 +0900

    centering class

--- examples/_test2/main.js
+++ examples/_test2/main.js
@@ -10,6 +10,9 @@
  *   or send a letter to Creative Commons, PO Box 1866,
  *   Mountain View, CA 94042, USA. 
  *
+ * This code is a modified version of the above work.
+ * hta39393
+ * Centering class, etc.
  */
 
 // Application
@@ -23,68 +26,128 @@
 
 import Modules from "modules";
 import Time from "timer";
+import Timer from "timer";
 
-const BLUE = "blue";
-const OpenSans24 = Style.template({ font: "24px Open Sans",
-  color: BLUE });
+const SCRW = 320;
+const SCRH = 240;
+const LINEH = 30;
 
+/** 値を書き換えるのでフリーズできなくていいみたい */
 const _misc = {
-  _val: `${1}`,
+  mode: 7,
 };
 
-const PortClass1 = Port.template($ => ({
-  top: 0, bottom: 180, left: 240, right: 0,
-  skin: new Skin({fill: 'gray'}),
+const cols = ['black', 'blue', '#ff0000',
+        //'magenta',
+        'purple',
+        'green', 'cyan', 'yellow', '#ffffff'];
+Object.freeze(cols);
+
+const PortClass1 = Port.template($ => {
+  const top = + ($.y || 0);
+  const bottom = SCRH - (top + $.height);
+  const left = + ($.x || 0);
+  const right = SCRW - (left + $.width);
+  return {
+    top, bottom, left, right,
+    skin: new Skin({fill: '#333333'}),
   Behavior: class extends Behavior {
-		onCreate(port, data) {
-			this.data = data;
+		onCreate(target, data) {
 		}
-		onDraw(port) {
+		onDraw(target) {
 			let x = 0, y = 0;
 			{
-				port.fillColor('black', x, y, 16, 16);
-				x += 16;
-				y += 16;
+				//target.fillColor('black', x, y, SCRW, LINEH);
 			}
-      let text = `${this.data._val}`;
-      
-      //let size = _style1.measure(text);
+      {
+        x = SCRW / 4;
+        target.fillColor('#666666', x, y, SCRW * 0.5, LINEH);
+      }
+      let text = target.string;
+
       let size = _misc._style1.measure(text);
       const w = size.width;
       const h = size.height;
-      port.drawStyle(text, _misc._style1, x, y, w, h, false, 1);
+      x = ($.width - w) / 2;
+      //y = ($.height - h) / 2;
+      //y = LINEH * 0.5;
+      //target.drawString(text, _misc._style1, 'white', x, y, w, h);
+      target.drawString(text, _misc._style1, cols[7], x, y, w, h);
+      //target.drawStyle(text, _misc._style1, x, y, w, h, false, 7);
+
+      //target.delegate('func', 1, 2, 3);
 		}
+
 	}
-}));
+
+  };
+});
+
+const PortClass2 = Port.template($ => {
+  const top = + ($.y || 0);
+  const bottom = SCRH - (top + $.height);
+  const left = + ($.x || 0);
+  const right = SCRW - (left + $.width);
+  const frontcol = $.frontcol ?? '#ffffff';
+  const backcol = $.backcol ?? '#000000';
+  const style = new Style({font: "24px Open Sans"});
+  return {
+    top, bottom, left, right,
+    skin: new Skin({fill: backcol}),
+    Behavior: class extends Behavior {
+      onCreate(target, data) {
+        target.string = data?.string || '';
+      }
+      onDraw(target) {
+        let y = 0;
+        let text = target.string;
+        let size = style.measure(text);
+        const w = size.width;
+        const h = size.height;
+        let x = ($.width - w) / 2;
+        //y = ($.height - h) / 2;
+        //y = LINEH * 0.5;
+        target.drawString(text, style, frontcol, x, y, w, h);
+      }
+    }
+  };
+});
 
 const _fcontainer = ($) => {
+  trace(`_fcontainer, ${$?._foo}\n`);
   const ret = {
-    Skin: BackgroundSkin, left: 0, right: 0, top: 0, bottom: 0,
-    contents: [
-      Text($, {left: 0, right: 0, Style: OpenSans24, string: `gui\ngui!`}),
-      Text($, {left: 0, right: 160, Style: OpenSans24, string: `2`}),
-      new PortClass1(_misc),
-    ]
+    Skin: BackgroundSkin,
+    left: 0, right: 0, top: 0, bottom: 0,
+    contents: [],
   };
+  _misc.y = SCRH - LINEH;
+  _misc.width = SCRW;
+  _misc.height = LINEH;
+  ret.contents.push(new PortClass1(_misc));
+
+  ret.contents.push(new PortClass2({
+    x: SCRW * 0.5,
+    y: LINEH * 2,
+    width: SCRW * 0.25,
+    height: LINEH,
+    frontcol: '#ccc',
+    backcol: '#000033',
+  }));
   _misc.contents = ret.contents;
   return ret;
 };
 
-
-const WHITE = "white";
-
-const BackgroundSkin = Skin.template({ fill: 'red' });
+const BackgroundSkin = Skin.template({ fill: '#000000' });
 
 class MediaBehavior extends Behavior {
   onCreate(application, data) {
     this.data = data;
 
     const _style1 = new Style({ font: "24px Open Sans",
-      color: [BLUE, 'yellow', 'green'] });
+      color: cols });
     _misc._style1 = _style1;
 
-    application.add(new NoModUI());
-    trace(`no UI 235\n`);
+    application.add(new NoModUI({_foo: 'bar'}));
 
     // 0-6
     this.mode = 0;
@@ -102,6 +165,13 @@ class MediaBehavior extends Behavior {
           if (up === 1) {
             _this.mode = (_this.mode + 1) % 7;
             _this.setNP(_this.npcols[_this.mode + 1]);
+            _misc.mode = + _this.mode;
+
+            const port1 = _misc.contents[1];
+            if (port1) {
+              port1.string = `${_misc.mode * 100}`;
+              port1.invalidate();
+            }
           }
         }
       } else {
@@ -112,6 +182,12 @@ class MediaBehavior extends Behavior {
           const up = this.read();
 
           trace(`b up ${up}`);
+
+          if (up === 1) {
+            const port1 = _misc.contents[1];
+            port1.string = `456`;
+            port1.invalidate();
+          }
         }
       }
       if (c) {
@@ -123,10 +199,10 @@ class MediaBehavior extends Behavior {
             _this.setNP(_this.npcols[1]);
 
             trace (`c release, ${_misc.contents.length}`);
-            const port1 = _misc.contents[2];
-            if (port1) {
-              _misc._val = `123`;
-              port1.invalidate();
+            const port0 = _misc.contents[0];
+            if (port0) {
+              port0.string = `123`;
+              port0.invalidate();
             }
           }
         }
@@ -142,7 +218,11 @@ class MediaBehavior extends Behavior {
 
     }
 
-
+    Timer.set(() => {
+      const port1 = _misc.contents[1];
+      port1.string = `456`;
+      port1.invalidate();
+    }, 1000);
   }
 
   setNP(col) {
